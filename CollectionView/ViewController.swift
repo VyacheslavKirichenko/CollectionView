4ac772b0863e4b8430114e923cb2475ec69e71ed
VyacheslavKirichenko <prostoslava321@gmail.com>
2019-05-05 03:07:39 -0700

    Update

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -90,6 +90,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     
+    
+    
+    
+    
+    
+    
+    
+    // Create my Data
     func generationArrayData() {
         let imageArray = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "9"),UIImage(named: "10"),UIImage(named: "11")]
         let nameArray = ["Niva","LandCruiser 80","Hilux","FJ Cruiser","Wrangler","BRP Outlander","Yamaha Grizzly",
